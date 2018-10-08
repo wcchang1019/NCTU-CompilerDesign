ff90709ae8cc95152972b5ad0902c4f9e082453f
wcchang1019 <destiny10191019@gmail.com>
2018-10-08 13:21:57 -0700

    fix ID begin with underline

--- HW1/test.c
+++ HW1/test.c
@@ -1,13 +1,22 @@
+/* 
+3
+1
+2
+2
+2
+*/
 int main(int argc, char **argv) {
     a = 0.000055454;
     a = 0123123.123123; 
-    b = "aa \"bb \\cc";
+    b = "aa \\\"\\bb \\\"\\cc  \\\\\" \"\\\\"
     c = 01.23E4;
     c = 0001.23e4;
+    !=_haha
+    ha_hahaha
 #pragma statistic off
     c = 00000.0000e-45;
-#pragma token off //comments
-#pragma source off /* comment
-s */
+
 #pragma statistic on
 }
+
+// ///**/*65a5sdasdasd
