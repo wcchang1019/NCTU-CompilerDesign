60ba0b0c9862d450c78f670a3fbf4c317c3c6fe4
wcchang1019 <destiny10191019@gmail.com>
2018-10-10 15:49:19 +0800

    add HW1 README

--- HW1/test.c
+++ HW1/test.c
@@ -1,22 +1,12 @@
-/* 
-3
-1
-2
-2
-2
-*/
-int main(int argc, char **argv) {
-    a = 0.000055454;
-    a = 0123123.123123; 
-    b = "aa \\\"\\bb \\\"\\cc  \\\\\" \"\\\\"
-    c = 01.23E4;
-    c = 0001.23e4;
-    !=_haha
-    ha_hahaha
-#pragma statistic off
-    c = 00000.0000e-45;
-
-#pragma statistic on
+// print hello world
+void main()
+{
+int a;
+double b;
+print "hello world";
+a = 1+1;
+b = 1.23;
+if (a > 1){
+b = b*1.23e-1;
 }
-
-// ///**/*65a5sdasdasd
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NCTU-CompilerDesign
+## HW1 - Lexical Definition
+- Abilities
+    analyze string into token which defined in document
+- Platform 
+    the linux workstaion of CS
+- How to run
+    ```
+    $ make
+    $ ./scanner test.c
+    ```
