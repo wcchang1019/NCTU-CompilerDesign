3a8b024bd32b36f86c4311a150c36a876bd70ae2
wcchang1019 <destiny10191019@gmail.com>
2018-10-11 01:51:50 +0800

    fix scientific bug

--- HW1/test.c
+++ HW1/test.c
@@ -1,18 +1,1 @@
-// print hello world
-#pragma source on //fafaf 
-/*sdfsdfsdfsd
-dsfsdff*/
-#pragma token
-on /*sdfsdfsdfsdfsdf
-sdfsdfsdf*/
-void main()
-{
-int a;
-double b;
-print "hello world";
-a = 1+1;
-b = 1.23;
-if (a > 1){
-b = b*1.23e-1;
-}
-}
+000000E+04
