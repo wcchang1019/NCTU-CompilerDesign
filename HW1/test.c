a78bcdb9b249bdaa9c48124aa0be6b211f9b80e9
wcchang1019 <destiny10191019@gmail.com>
2018-10-12 02:36:37 +0800

    fix pragma bug

--- HW1/test.c
+++ HW1/test.c
@@ -1,1 +1,7 @@
-000000E+04
+#pragma source on               /////12313134564                    /* dfs
+fd*/
+#pragma token on                               // asdasdasd/*sdfsdfsdf*/
+#pragma statistic on          321321321321321321321
+a
+b
+b
